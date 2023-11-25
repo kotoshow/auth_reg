f496dfc1d554ed458a8658421f4d2f757fa8d069
n.mshecyan <43683504+kotoshow@users.noreply.github.com>
2023-11-25 15:28:34 +0300

    deploy

---
+++ script-reg.js
@@ -0,0 +1,21 @@
+let submitBtn = document.getElementById("form-submit");
+let passVisible = document.getElementById("pass-show");
+
+function passShow() {
+    let passVisibleIcon = document.getElementsByClassName("icon-password-visible")[0];
+    if (passVisibleIcon.classList.contains("icon-password-show")) {
+        passVisibleIcon.classList.remove("icon-password-show");
+        document.getElementById("password-input").setAttribute("type", "password");
+    }
+    else {
+        passVisibleIcon.classList.add("icon-password-show");
+        document.getElementById("password-input").setAttribute("type", "text");
+    }
+};
+
+function test() {
+    alert(document.getElementById("username-input").value);
+}
+
+//submitBtn.addEventListener("click", test);
+passVisible.addEventListener("click", test);
